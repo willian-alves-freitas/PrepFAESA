0b5b7206febd5760778d4007219389e669a519ff
Willian Alves Freitas Foca <alveswillian794@gmail.com>
2025-12-01 20:29:49 -0300

    Recriação do commit inicial

---
+++ src/main/java/com/company/quizopedia/view/user/UserListView.java
@@ -0,0 +1,14 @@
+package com.company.quizopedia.view.user;
+
+import com.company.quizopedia.entity.User;
+import com.company.quizopedia.view.main.MainView;
+import com.vaadin.flow.router.Route;
+import io.jmix.flowui.view.*;
+
+@Route(value = "users", layout = MainView.class)
+@ViewController(id = "User.list")
+@ViewDescriptor(path = "user-list-view.xml")
+@LookupComponent("usersDataGrid")
+@DialogMode(width = "64em")
+public class UserListView extends StandardListView<User> {
+}
